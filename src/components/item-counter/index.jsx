eb138cfabc65d8ea25e64bb3ba8f665de6a179d3
marvillalba9 <maruja.villalba@gmail.com>
2023-07-02 20:04:53 -0300

    cards para productos

---
+++ src/components/item-counter/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react';
+
+const ItemCounter = ({stock, initial, onAdd}) => {
+  const [quantity, setQuantity] = useState (initial);
+
+  const increment = () => {
+    if (quantity < stock) {
+        setQuantity(quantity + 1)
+    }
+  }
+
+  const decrement = () => {
+    if (quantity > initial) {
+      setQuantity(quantity - 1);
+    }
+  }
+
+    return (
+        <div className='counter'>
+            <div className='controls'>
+                <button className='button' onClick={decrement}>-</button>
+                <h4 className='number'>{quantity}</h4>
+                <button className='button' onClick={increment}>+</button>
+            </div>
+            <div>
+                <button className='button' onClick={() => onAdd(quantity)} disabled={!stock}>
+                    Agregar al carrito 
+                </button>
+            </div>
+        </div>
+  )
+}
+
+export default ItemCounter;
+
+//         <ItemCounter initial={1} stock={10} onAdd={(quantity) =>console.log('Cantidad agregada ', quantity)}/>
+
