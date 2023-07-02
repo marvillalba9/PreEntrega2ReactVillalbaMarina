d59bba41d75c3d418551cb0cf70f27dcf9105af5
marvillalba9 <maruja.villalba@gmail.com>
2023-07-02 15:22:49 -0300

    pre-entrega2react

---
+++ src/pages/main/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResponsiveAppBar from '../../components/navbar';
+//import NavBar from '../../components/navbar/index.jsx';
+//import ItemList from '../../components/itemlist/index.jsx';
+
+const Main = () => {
+    return (
+        <div className='contenedor'>
+            <ResponsiveAppBar />
+        </div>
+    )
+}
+
+export default Main;
